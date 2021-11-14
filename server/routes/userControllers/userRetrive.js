d63b3023dc33fab62290740470055f5715e10f03
Ritesh Rai <riteshrai997@gmail.com>
2021-11-14 17:40:08 +0530

    profile route config in userRetrive controller

--- server/routes/userControllers/userRetrive.js
+++ server/routes/userControllers/userRetrive.js
@@ -3,6 +3,7 @@ const expressAsyncHandler = require("express-async-handler");
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const isAuth = require("../../middlewares/isAuth.js");
+const generateToken = require("../../middlewares/jwtVerify");
 
 const userRetrive = express.Router();
 
@@ -17,6 +18,28 @@ userRetrive.get(
       });
     }
     res.status(200).json(user);
+  })
+);
+userRetrive.put(
+  "/profile",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    }
   })
 );
 
