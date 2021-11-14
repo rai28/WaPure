de3b5dded94dc58597a66c7f16cedf2acbb8bfd9
Ritesh Rai <riteshrai997@gmail.com>
2021-11-14 17:35:42 +0530

    user model modified

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const UsersSchema = new mongoose.Schema(
   {
-    userName: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
